884d37e3f984b05775c8a1f7bf416659391800b4
yeongwoo <jyy0223@naver.com>
2024-09-18 23:34:11 +0900

    [feat] loading, error 컴포넌트 추가

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import {sleep} from "@/lib/utils";
+
+const home = async function Home() {
+  await sleep(2000);
+  // throw new Error("ERROR");
   return <div>HOME</div>;
-}
+};
+
+export default home;
---
+++ components/LoadingBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BarLoader} from "react-spinners";
+
+const LoadingBar = () => {
+  return (
+    <div className="w-full">
+      <BarLoader color='red' cssOverride={{width: "100%"}}/>
+    </div>
+  );
+};
+
+export default LoadingBar;
