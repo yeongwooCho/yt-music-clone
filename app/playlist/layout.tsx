729ec7f66e7e16c90ccabb6f6b04d6483ec0e5c2
yeongwoo <jyy0223@naver.com>
2024-09-19 15:46:42 +0900

    [chore] layout에서 받는 children의 타입을 지정 - {children: React.ReactNode}

--- app/playlist/layout.tsx
+++ app/playlist/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const layout = ({children}) => {
+const layout = ({children}: { children: React.ReactNode }) => {
   return (
     <div>
       layout 이 먼저
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from "@/components/elements/Logo";
 import Navigator from "@/components/elements/Navigator";
 
-const Sidebar = ({children}) => {
+const Sidebar = ({children}: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-row h-full ">
       <nav className="w-[240px] border-r-[1px] border-neutral-600">
