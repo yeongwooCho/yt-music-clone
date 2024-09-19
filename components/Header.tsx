c6702046add3a89103a66c59ce8f5e575a4e9d96
yeongwoo <jyy0223@naver.com>
2024-09-19 17:41:08 +0900

    [feat] Header component 추가: 외부 이미지 링크 사용 방법과 next/image component 사용 방법, absolute, relative 적용

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from "next/image";
+
+const Header = ({children}: { children: React.ReactNode }) => {
+  return (
+    <header className="relative overflow-y-auto w-full h-full">
+      <section className="absolute top-0 w-full">
+        <div className="relative h-[400px] w-full">
+          <Image fill src="https://images.unsplash.com/photo-1487956382158-bb926046304a" alt="bg-image"
+                 className="object-cover"/>
+          {/*<div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black"/>*/}
+          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black"/>
+        </div>
+      </section>
+      <section className="absolute">
+        {children}
+      </section>
+    </header>
+  );
+};
+
+export default Header;
