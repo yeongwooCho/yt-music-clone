918b53fb4779258224c71c2c5379f38d5b6a20f0
yeongwoo <jyy0223@naver.com>
2024-09-22 17:53:55 +0900

    [feat] 둘러보기 화면 구현

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import PagePadding from "@/components/PagePadding";
 import Category from "@/app/explore/components/Category";
+import PlayListCarousel from "@/components/PlayListCarousel";
+import UserIcon from "@/components/UserIcon";
+import {getAllPlaylist} from "@/lib/dummyData";
 
-const page = () => {
+const page = async () => {
+  const playListArray = await getAllPlaylist();
+
   return (
     <PagePadding>
       <div>
         <Category></Category>
+
+        <div className="mt-20"></div>
+
+        <PlayListCarousel
+          title="커버 및 리믹스"
+          subTitle=""
+          playListArray={playListArray}
+          Thumbnail={(
+            <div className="w-[56px] h-[56px]">
+              <UserIcon size="lg"/>
+            </div>
+          )}/>
       </div>
     </PagePadding>
 
--- components/PlayListCarousel.tsx
+++ components/PlayListCarousel.tsx
@@ -38,7 +38,7 @@ const PlayListCarousel: React.FC<IPlayListCarouselProps> = ({
         <CarouselContent className="mt-4">
           {playListArray?.map((playList, index) => {
             return (
-              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 xl:basis-1/5">
+              <CarouselItem key={index} className="basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5">
                 <div className="p-1">
                   <PlayListCard key={index} playlist={playList}></PlayListCard>
                 </div>
