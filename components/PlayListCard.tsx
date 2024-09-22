914e5ecd0ef3c9802dac27b91cc4012397697627
yeongwoo <jyy0223@naver.com>
2024-09-22 15:12:54 +0900

    [feat] PlayListCard hover시 변경되는 css 추가

--- components/PlayListCard.tsx
+++ components/PlayListCard.tsx
@@ -5,6 +5,10 @@ import {IPlayList, ISong} from "@/types";
 import Image from 'next/image';
 import {getRandomElementFromArray} from "@/lib/utils";
 import {useRouter} from "next/navigation";
+import {MdMoreVert} from "react-icons/md";
+import {FiPlay} from "react-icons/fi";
+import IconButton from "@/components/elements/IconButton";
+import {red} from "next/dist/lib/picocolors";
 
 const PlayListCard = ({playlist}: { playlist: IPlayList }) => {
   const {push} = useRouter();
@@ -16,10 +20,27 @@ const PlayListCard = ({playlist}: { playlist: IPlayList }) => {
     push(`/playlist?list=${id}`);
   }
 
+  const onClickPlay = () => {
+    // TODO: play song
+    console.log('play song');
+  }
+
   return (
-    <article className="h-[240px] cursor-pointer" onClick={onClickCard}>
+    <article className="group h-[240px] cursor-pointer" onClick={onClickCard}>
       <section className="relative h-[136px]">
         <Image src={randomSong.imageSrc} alt='thumbnail' fill={true} className="object-cover"/>
+        <div className="hidden relative group-hover:block bg-gradient-to-b from-[rgba(0,0,0,0.7)] top-0 w-full h-[136px]">
+          <div className="absolute top-2 right-3">
+            <IconButton icon={
+              <MdMoreVert size={20}/>
+            }/>
+          </div>
+          <div className="absolute bottom-2 right-2 flex items-center justify-center transform-gpu
+          transition-transform hover:scale-110 rgba(0,0,0,0.7) hover:bg-[rgba(0,0,0,0.9)] h-[45px] w-[45px] rounded-full
+          pl-1" onClick={onClickPlay}>
+            <FiPlay size={20} color="red"/>
+          </div>
+        </div>
       </section>
       <section>
         <div className="mt-2">{playlistName}</div>
