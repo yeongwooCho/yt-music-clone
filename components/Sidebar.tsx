667219b770cb8f19e2ea5c0f57d45394f475671b
yeongwoo <jyy0223@naver.com>
2024-09-23 20:55:28 +0900

    [feat] side bar size 변경: calc

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,17 +6,17 @@ const Sidebar = ({children}: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-row h-full ">
       <nav className="hidden lg:block w-[240px] border-r-[1px] border-neutral-600">
-        <div className="p-1.5">
+        <div className="p-[24px]">
           <Logo/>
         </div>
         <div>
           <Navigator/>
         </div>
       </nav>
-      <div className="flex-1">
+      {/*<div className="flex-1">*/}
+      <div className="w-full lg:w-[calc(100%-240px)]">
         {children}
       </div>
-
     </div>
   );
 };
