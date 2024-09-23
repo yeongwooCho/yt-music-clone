3ba904bccb1afb49970b24dd398d956abbea8092
yeongwoo <jyy0223@naver.com>
2024-09-24 02:11:28 +0900

    [feat] props 변수로 객체를 받는 방법과 잘못된 URI 접근 시 permanentRedirect 로 접근하는 로직 구현

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {getPlaylistById} from "@/lib/dummyData";
+import {permanentRedirect} from "next/navigation";
 
-const page = (props: { searchParams: { list: string } }) => {
-  console.log(props);
+interface IPlayListPageProps {
+  searchParams: { list: string };
+}
+
+const page = async (props: IPlayListPageProps) => {
+  const playlist = await getPlaylistById(Number(props.searchParams.list));
+
+  if (!playlist) {
+    permanentRedirect("/"); // 홈으로 이동
+  }
+
   return (
     <div>
-      playlist: {props.searchParams.list}
+      playlist: {JSON.stringify(playlist)}
     </div>
   );
 };
